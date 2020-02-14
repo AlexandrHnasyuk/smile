af5c1dc74ed137d6b8695a06c8ddc9f2c1bcc627
Alexandr <alexandr.h@rocksolid.com.ua>
2020-02-14 13:49:00 +0200

    Started task with views. #12

---
+++ web/modules/custom/views_pets/src/Plugin/views/filter/NodePrefix.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Drupal\views_pets\Plugin\views\filter;
+
+use Drupal\Core\Entity\Query\Sql\Condition;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\views\Plugin\views\filter\FilterPluginBase;
+
+/**
+ * Filters by given list of node title options.
+ *
+ * @ingroup views_filter_handlers
+ *
+ * @ViewsFilter("views_node_prefix")
+ */
+class NodePrefix extends FilterPluginBase {
+  /**
+   * {@inheritdoc}
+   */
+
+}
